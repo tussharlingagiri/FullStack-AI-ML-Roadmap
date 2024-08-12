a827b5eecea00e011a4f6648f4197d17c6acd42c
tussharlingagiri <117937903+dragonemperor-ulti@users.noreply.github.com>
2024-08-12 15:48:42 +0100

    second commit

---
+++ simple_calculator.js
@@ -0,0 +1,28 @@
+function calculate(operation){
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    let result;
+
+    if (isNaN(num1) || isNaN(num2)){
+        result = "Please enter valid numbers";
+    } else {
+        switch (operation){
+            case 'add':
+                result = num1 + num2;
+                break;
+            case 'subtract':
+                result = num1 - num2;
+                break;
+            case 'multiply':
+                result = num1*num2;
+                break;
+            case 'divide':
+                result = num2 !== 0 ? num1/num2 : 'Cannot divide by zero';
+                break;
+            default:
+                result = 'Invalid Operation';
+        }
+        
+    }
+    document.getElementById('result').textContent = result;
+}
